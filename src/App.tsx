017233a1a7defd34de314b3b556212a993be0bdb
a-bos95 <andrea.boschetto@akqa.com>
2024-12-05 18:00:51 +0100

    further improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useRef, useEffect, useState } from 'react';
 import Container from './components/Container';
 import SearchBar from './components/SearchBar';
 import SortDropdown from './components/SortDropdown';
@@ -8,24 +7,15 @@ import PokemonCard from './components/PokemonCard';
 import PokemonDetail from './components/PokemonDetail';
 
 function App() {
-  const searchBarRef = useRef<HTMLDivElement>(null);
-  const [spacerHeight, setSpacerHeight] = useState(0);
-
-  useEffect(() => {
-    if (searchBarRef.current) {
-      setSpacerHeight(searchBarRef.current.offsetHeight);
-    }
-  }, []);
-
   return (
     <Container type="div" className="border-2 border-red-500">
       <div className="flex gap-8 border-2 border-blue-500">
         {/* Left Column */}
         <Container type="div" className="flex-1 border-2 border-yellow-500">
-          <div ref={searchBarRef}>
+          <Container type="div" className="border-2 border-green-500">
             <SearchBar />
-          </div>
-          <div className="border-2 border-purple-500">
+          </Container>
+          <Container type="div" className="border-2 border-purple-500">
             <div className="flex items-center justify-between">
               <SortDropdown />
               <RangeInput />
@@ -36,12 +26,11 @@ function App() {
               <PokemonCard number="388" name="Grotle" types={['Grass']} />
               <PokemonCard number="389" name="Torterra" types={['Grass', 'Ground']} />
             </div>
-          </div>
+          </Container>
         </Container>
 
         {/* Right Column */}
-        <Container type="div" className="w-[35%] border-2 border-pink-500">
-          <div style={{ height: spacerHeight }} className="border-2 border-green-500" />
+        <Container type="aside" className="w-[35%] border-2 border-pink-500 self-start mt-[SearchBarHeight]">
           <PokemonDetail />
         </Container>
       </div>
